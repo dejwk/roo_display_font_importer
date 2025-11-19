31b14eccec6b89e62c0e36410bc5e5f0d40c3952
Dawid Kurzyniec <dawid.kurzyniec@gmail.com>
2025-11-19 20:06:38 +0100

    Added support for uncompressed glyphs.

--- app/src/main/java/roo/display/FontEncoder.java
+++ app/src/main/java/roo/display/FontEncoder.java
@@ -1,13 +1,12 @@
 package roo.display;
 
+import hexwriter.HexWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.Writer;
 import java.util.Date;
 import java.util.List;
-
-import hexwriter.HexWriter;
 import roo.display.RooDisplayFont.Glyph;
 import roo.display.encode.*;
 import roo.display.encode.alpha4.*;
@@ -16,21 +15,29 @@ class FontEncoder {
   boolean rle;
   final RooDisplayFont font;
 
-  public FontEncoder(RooDisplayFont font) {
-    this.font = font;
-  }
+  public FontEncoder(RooDisplayFont font) { this.font = font; }
 
-  public RooDisplayFont getFont() {
-    return font;
-  }
+  public RooDisplayFont getFont() { return font; }
 
   public void writeDeclaration(Writer os, String var) throws IOException {
     HexWriter hexWriter = new HexWriter(os);
-    hexWriter.printComment("Font " + font.getFont().getPSName() + " (" + font.getFont().getName() + ")\n");
+    hexWriter.printComment("Font " + font.getFont().getPSName() + " (" +
+                           font.getFont().getName() + ")\n");
     os.write("const Font& " + var + "();");
   }
 
-  public int writeDefinition(Writer os, String var, boolean rle) throws IOException {
+  static class EncodedGlyph {
+    public EncodedGlyph(byte[] data, boolean compressed) {
+      this.data = data;
+      this.compressed = compressed;
+    }
+
+    byte[] data;
+    boolean compressed;
+  };
+
+  public int writeDefinition(Writer os, String var, boolean rle)
+      throws IOException {
     final RooDisplayFont.MaxFontSize maxFontSize;
     HexWriter hexWriter = new HexWriter(os);
     GlyphEncoder glyphEncoder = new GlyphEncoder(font.getAlphaBits(), rle);
@@ -38,7 +45,7 @@ class FontEncoder {
 
     // Actually encode all glyphs. We need this to know the sizes in advance, to
     // generate offsets.
-    byte[][] encodedGlyphs = new byte[glyphs.size()][];
+    EncodedGlyph[] encodedGlyphs = new EncodedGlyph[glyphs.size()];
     for (int i = 0; i < glyphs.size(); ++i) {
       encodedGlyphs[i] = glyphEncoder.encodeGlyph(glyphs.get(i));
     }
@@ -47,10 +54,12 @@ class FontEncoder {
     int maxOffset = 0;
     if (glyphs.size() > 0) {
       for (int i = 0; i < glyphs.size() - 1; ++i) {
-        maxOffset += encodedGlyphs[i].length;
+        maxOffset += encodedGlyphs[i].data.length;
       }
     }
-    final int offsetBytes = (maxOffset < (1 << 8)) ? 1 : (maxOffset < (1 << 16)) ? 2 : 3;
+    final int offsetBytes = (maxOffset < (1 << 8))    ? 1
+                            : (maxOffset < (1 << 16)) ? 2
+                                                      : 3;
     // OffsetWriter offsetWriter = new OffsetWriter(offsetBytes, hexWriter);
 
     int maxFontMetricBytes = 1;
@@ -64,25 +73,30 @@ class FontEncoder {
       maxBoundingBox = glyph.getBoundingBox().expand(maxBoundingBox);
       minAdvance = Math.min(minAdvance, glyph.getAdvance());
       maxAdvance = Math.max(maxAdvance, glyph.getAdvance());
-      maxRightOverhang = Math.max(
-          maxRightOverhang,
-          glyph.getAdvance() - glyph.getBoundingBox().xMax + 1);
+      maxRightOverhang =
+          Math.max(maxRightOverhang,
+                   glyph.getAdvance() - glyph.getBoundingBox().xMax + 1);
       if (glyph.getCodePoint() == 'i') {
         defaultSpaceAdvance = glyph.getAdvance();
       }
     }
 
-    boolean fixedPoint = (minAdvance == maxAdvance && font.getKerningPairs().isEmpty());
+    boolean fixedPoint =
+        (minAdvance == maxAdvance && font.getKerningPairs().isEmpty());
     // int linesep = (int) Math.round(0.35 * (font.getAscent() +
     // font.getDescent()));
-    int linesep = Math.max(
-        maxBoundingBox.getHeight() - (font.getAscent() - font.getDescent()),
-        (int) (0.2 * (font.getAscent() + font.getDescent())));
+    int linesep = Math.max(maxBoundingBox.getHeight() -
+                               (font.getAscent() - font.getDescent()),
+                           (int)(0.2 * (font.getAscent() + font.getDescent())));
 
-    maxFontMetricBytes = Math.max(maxFontMetricBytes, signedBytes(maxBoundingBox.xMin));
-    maxFontMetricBytes = Math.max(maxFontMetricBytes, signedBytes(maxBoundingBox.xMax));
-    maxFontMetricBytes = Math.max(maxFontMetricBytes, signedBytes(maxBoundingBox.yMin));
-    maxFontMetricBytes = Math.max(maxFontMetricBytes, signedBytes(maxBoundingBox.yMax));
+    maxFontMetricBytes =
+        Math.max(maxFontMetricBytes, signedBytes(maxBoundingBox.xMin * 2));
+    maxFontMetricBytes =
+        Math.max(maxFontMetricBytes, signedBytes(maxBoundingBox.xMax));
+    maxFontMetricBytes =
+        Math.max(maxFontMetricBytes, signedBytes(maxBoundingBox.yMin));
+    maxFontMetricBytes =
+        Math.max(maxFontMetricBytes, signedBytes(maxBoundingBox.yMax));
     maxFontMetricBytes = Math.max(maxFontMetricBytes, signedBytes(maxAdvance));
 
     if (defaultSpaceAdvance == 0) {
@@ -90,18 +104,21 @@ class FontEncoder {
       defaultSpaceAdvance = font.getAscent() / 2;
     }
 
-    FontMetricWriter metricWriter = new FontMetricWriter(maxFontMetricBytes, hexWriter);
+    FontMetricWriter metricWriter =
+        new FontMetricWriter(maxFontMetricBytes, hexWriter);
     OffsetWriter offsetWriter = new OffsetWriter(offsetBytes, hexWriter);
 
-    hexWriter.printComment("Font " + font.getFont().getPSName() + " (" + font.getFont().getName() + ")\n");
+    hexWriter.printComment("Font " + font.getFont().getPSName() + " (" +
+                           font.getFont().getName() + ")\n");
     hexWriter.printComment("Generated on " + new Date() + "\n");
     hexWriter.beginStatic(var);
     hexWriter.newLine();
     hexWriter.printComment("Header");
     hexWriter.newLine();
-    hexWriter.printHex16(0x0101);
+    hexWriter.printHex16(0x0102);
     hexWriter.printHex8(font.getAlphaBits().bits());
-    hexWriter.printHex8(font.getCharset() == RooDisplayFont.Charset.ASCII ? 1 : 2);
+    hexWriter.printHex8(font.getCharset() == RooDisplayFont.Charset.ASCII ? 1
+                                                                          : 2);
     hexWriter.printHex8(maxFontMetricBytes);
     hexWriter.printHex8(offsetBytes);
     hexWriter.printHex8(rle ? 0x01 : 0x00);
@@ -123,14 +140,14 @@ class FontEncoder {
 
     // Default glyph to substitute if a requested glyph is missing.
     switch (font.getCharset()) {
-      case ASCII: {
-        hexWriter.printHex8('_');
-        break;
-      }
-      case UTF8: {
-        hexWriter.printHex16('_');
-        break;
-      }
+    case ASCII: {
+      hexWriter.printHex8('_');
+      break;
+    }
+    case UTF8: {
+      hexWriter.printHex16('_');
+      break;
+    }
     }
 
     hexWriter.newLine();
@@ -142,26 +159,42 @@ class FontEncoder {
       RooDisplayFont.Glyph glyph = glyphs.get(i);
       hexWriter.newLine();
       switch (font.getCharset()) {
-        case ASCII:
-          hexWriter.printHex8(glyph.getCodePoint());
-          break;
-        case UTF8:
-          hexWriter.printHex16(glyph.getCodePoint());
-          break;
+      case ASCII:
+        hexWriter.printHex8(glyph.getCodePoint());
+        break;
+      case UTF8:
+        hexWriter.printHex16(glyph.getCodePoint());
+        break;
       }
       RooDisplayFont.BoundingBox boundingBox = glyph.getBoundingBox();
-      metricWriter.print(boundingBox.xMin);
+      // Encode the information whether the glyph is RLE-compressed in the
+      // xMin field's MSB (after the sign bit). xMin tends to be closest to
+      // zero, so this bit is least likely used.
+      //
+      // If the sign bit is equal to that high bit, it means that the glyph is
+      // compressed. if they are different, it means that the glyph is
+      // uncompressed.
+      //
+      // In practice, we have not seen a font which would have this bit ever be
+      // different than the sign bit so far. This means that this change is
+      // backwards-compatible with existing font files.
+      int val = boundingBox.xMin;
+      if (!encodedGlyphs[i].compressed) {
+        // Need to flip the bit.
+        val ^= (1 << (8 * maxFontMetricBytes - 2));
+      }
+      metricWriter.print(val);
       metricWriter.print(boundingBox.yMin);
       metricWriter.print(boundingBox.xMax);
       metricWriter.print(boundingBox.yMax);
       metricWriter.print(glyph.getAdvance());
       offsetWriter.print(currentOffset);
 
-      String comment = ("\"" + (char) glyph.getCodePoint() + "\"");
+      String comment = ("\"" + (char)glyph.getCodePoint() + "\"");
       comment += String.format(" (U+%04X)", glyph.getCodePoint());
       // Encode in UTF-8, because why not. It's just a comment.
       hexWriter.printComment(comment);
-      currentOffset += encodedGlyphs[i].length;
+      currentOffset += encodedGlyphs[i].data.length;
     }
 
     hexWriter.newLine();
@@ -171,22 +204,22 @@ class FontEncoder {
       RooDisplayFont.CodePointPair cp = i.codePoints;
       hexWriter.newLine();
       switch (font.getCharset()) {
-        case ASCII:
-          hexWriter.printHex8(cp.left);
-          hexWriter.printHex8(cp.right);
-          break;
-        case UTF8:
-          hexWriter.printHex16(cp.left);
-          hexWriter.printHex16(cp.right);
-          break;
+      case ASCII:
+        hexWriter.printHex8(cp.left);
+        hexWriter.printHex8(cp.right);
+        break;
+      case UTF8:
+        hexWriter.printHex16(cp.left);
+        hexWriter.printHex16(cp.right);
+        break;
       }
       if (i.kern < 1 || i.kern > 255) {
         throw new IllegalArgumentException("Kern outside range: " + i.kern);
       }
       hexWriter.printHex8(i.kern);
-      hexWriter
-          .printComment("" + (char) cp.left + (char) cp.right +
-              String.format(" (U+%04X U+%04X)", cp.left, cp.right));
+      hexWriter.printComment(
+          "" + (char)cp.left + (char)cp.right +
+          String.format(" (U+%04X U+%04X)", cp.left, cp.right));
     }
 
     hexWriter.newLine();
@@ -195,11 +228,11 @@ class FontEncoder {
     for (int i = 0; i < glyphs.size(); ++i) {
       RooDisplayFont.Glyph glyph = glyphs.get(i);
       hexWriter.newLine();
-      String comment = ("\"" + (char) glyph.getCodePoint() + "\"");
+      String comment = ("\"" + (char)glyph.getCodePoint() + "\"");
       comment += String.format(" (U+%04X)", glyph.getCodePoint());
       hexWriter.printComment(comment);
       hexWriter.newLine();
-      hexWriter.printBuffer(encodedGlyphs[i]);
+      hexWriter.printBuffer(encodedGlyphs[i].data);
     }
 
     hexWriter.end();
@@ -211,12 +244,13 @@ class FontEncoder {
     private final RooDisplayFont.AlphaBits alphaBits;
     private final boolean rle;
 
-    private static Encoder createEncoder(RooDisplayFont.AlphaBits alphaBits, boolean rle, OutputStream os) {
+    private static Encoder createEncoder(RooDisplayFont.AlphaBits alphaBits,
+                                         boolean rle, OutputStream os) {
       switch (alphaBits.bits()) {
-        case 4:
-          return new Alpha4EncoderFactory().create(rle, os);
-        default:
-          throw new UnsupportedOperationException();
+      case 4:
+        return new Alpha4EncoderFactory().create(rle, os);
+      default:
+        throw new UnsupportedOperationException();
       }
     }
 
@@ -225,11 +259,12 @@ class FontEncoder {
       this.rle = rle;
     }
 
-    public byte[] encodeGlyph(RooDisplayFont.Glyph glyph) {
+    public EncodedGlyph encodeGlyph(RooDisplayFont.Glyph glyph) {
       int width = glyph.getBoundingBox().getWidth();
       int height = glyph.getBoundingBox().getHeight();
       ByteArrayOutputStream os = new ByteArrayOutputStream();
-      Encoder encoder = createEncoder(alphaBits, rle, os);
+      // Always try non-RLE to see if it produces a smaller result.
+      Encoder encoder = createEncoder(alphaBits, false, os);
       try {
         for (int rowid = 0; rowid < height; ++rowid) {
           for (int colid = 0; colid < width; ++colid) {
@@ -240,7 +275,28 @@ class FontEncoder {
       } catch (IOException e) {
         throw new RuntimeException(e);
       }
-      return os.toByteArray();
+      byte[] result = os.toByteArray();
+      boolean compressed = false;
+      if (rle) {
+        os = new ByteArrayOutputStream();
+        encoder = createEncoder(alphaBits, rle, os);
+        try {
+          for (int rowid = 0; rowid < height; ++rowid) {
+            for (int colid = 0; colid < width; ++colid) {
+              encoder.encodePixel(glyph.getPixelColor(colid, rowid));
+            }
+          }
+          encoder.close();
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+        byte[] rleResult = os.toByteArray();
+        if (rleResult.length < result.length) {
+          result = rleResult;
+          compressed = true;
+        }
+      }
+      return new EncodedGlyph(result, compressed);
     }
   }
 
@@ -253,23 +309,21 @@ class FontEncoder {
       this.writer = writer;
     }
 
-    final int size() {
-      return fontMetricBytes;
-    }
+    final int size() { return fontMetricBytes; }
 
     void print(int metric) throws IOException {
       switch (fontMetricBytes) {
-        case 1:
-          writer.printHex8(metric & 0xFF);
-          break;
-        case 2:
-          writer.printHex16(metric & 0xFFFF);
-          break;
-        case 3:
-          writer.printHex24(metric & 0xFFFFFF);
-          break;
-        default:
-          throw new AssertionError();
+      case 1:
+        writer.printHex8(metric & 0xFF);
+        break;
+      case 2:
+        writer.printHex16(metric & 0xFFFF);
+        break;
+      case 3:
+        writer.printHex24(metric & 0xFFFFFF);
+        break;
+      default:
+        throw new AssertionError();
       }
     }
   }
@@ -285,20 +339,20 @@ class FontEncoder {
 
     void print(int offset) throws IOException {
       switch (offsetBytes) {
-        case 1: {
-          writer.printHex8(offset);
-          break;
-        }
-        case 2: {
-          writer.printHex16(offset);
-          break;
-        }
-        case 3: {
-          writer.printHex24(offset);
-          break;
-        }
-        default:
-          throw new AssertionError();
+      case 1: {
+        writer.printHex8(offset);
+        break;
+      }
+      case 2: {
+        writer.printHex16(offset);
+        break;
+      }
+      case 3: {
+        writer.printHex24(offset);
+        break;
+      }
+      default:
+        throw new AssertionError();
       }
     }
   }
@@ -325,5 +379,4 @@ class FontEncoder {
       return 3;
     return 4;
   }
-
 }
